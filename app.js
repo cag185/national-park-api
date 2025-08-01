0c09c1ec97e6ceb39c706d673ebf5dbc27c79499
cag185 <cag185@pitt.edu>
2025-08-01 17:11:49 -0500

    removed enviornment check

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on http://localhost:${PORT}");
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
