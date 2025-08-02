1ef79ef36f58685979000d94893c15ea821e3af8
cag185 <cag185@pitt.edu>
2025-08-02 10:10:49 -0500

    added changes for production build

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "Not found" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Global error handler
 app.use((err, req, res, next) => {
